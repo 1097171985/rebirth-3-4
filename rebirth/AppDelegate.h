f5ae2d70d0711ca84fb6d4d21ea10635a9da90db
侯帅 <houshuai@localhost>
2016-07-11 11:17:59 +0800

    Initial Commit

---
+++ rebirth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rebirth
+//
+//  Created by 侯帅 on 16/7/11.
+//  Copyright © 2016年 侯帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rebirth/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  rebirth
+//
+//  Created by 侯帅 on 16/7/11.
+//  Copyright © 2016年 侯帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
